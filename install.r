aa9dd3279f7781e00cae33333c82b794e57ce398
Adelaide Rhodes <adelaide.rhodes@cgrb.oregonstate.edu>
2017-10-19 16:09:15 -0700

    Update install.r

--- install.r
+++ install.r
@@ -1,4 +1,4 @@
 require(knitr)
-source("https://bioconductor.org/biocLite.R")
-biocLite("DESeq2", dependencies=TRUE)
-biocLite("BiocParallel", dependencies=TRUE)
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("DESeq2", dependencies=TRUE)
+#biocLite("BiocParallel", dependencies=TRUE)
